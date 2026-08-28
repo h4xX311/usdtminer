cd5cfee9e91e7396129946df859e91b1d9535313
h4xX311 <96394056+h4xX311@users.noreply.github.com>
2026-08-28 11:37:14 -0400

    Add configuration file for project settings

---
+++ js/config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+    PROJECT_ID: "ad2ffb0bad081291b773d8c547c361b7", // Pega aquí tu ID de Reown Cloud
+    CHAIN_ID: "0x38",
+    CHAIN_NAME: "BNB Smart Chain",
+    BLOCK_EXPLORER: "https://bscscan.com",
+    RPC_URLS: [
+        "https://bsc-dataseed.binance.org/",
+        "https://bsc-dataseed1.defibit.io/",
+        "https://bsc-dataseed1.ninicoin.io/"
+    ],
+    USDT_ADDRESS: "0x55d398326f99059fF775485246999027B3197955",
+    MERCHANT_ADDRESS: "0x6253fecbb48a6a7d19f1b9a799e65fae58ab9b3b",
+    CONTRACT_ADDRESS: "0x8e18bE616f10565A63cEa65585Ddf1Ca61f1C634",
+    BACKEND_URL: "https://secure-merchant.onrender.com/api"
+};
