fa09d54df1fc551ec6ffaafe6f5ad84ce2fa1198
h4xX311 <96394056+h4xX311@users.noreply.github.com>
2026-08-29 10:36:11 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // js/app.js
 import { CONFIG } from './config.js';
-import { createAppKit } from 'https://esm.sh/@reown/appkit@latest';
-import { EthersAdapter } from 'https://esm.sh/@reown/appkit-adapter-ethers@latest';
+import { createAppKit } from 'https://cdn.jsdelivr.net/npm/@reown/appkit@1.6.0/+esm';
+import { EthersAdapter } from 'https://cdn.jsdelivr.net/npm/@reown/appkit-adapter-ethers@1.6.0/+esm';
 import { bsc } from 'https://esm.sh/@reown/appkit/networks';
 
 let modal = null;
