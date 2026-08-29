54ecd37914006181d459cdbbb9287f39a337e28d
h4xX311 <96394056+h4xX311@users.noreply.github.com>
2026-08-29 11:35:13 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { CONFIG } from './config.js';
-import { createAppKit } from 'https://esm.sh/@reown/appkit@1.6.0';
-import { Ethers5Adapter } from 'https://esm.sh/@reown/appkit-adapter-ethers5@1.6.0';
-import { bsc } from 'https://esm.sh/@reown/appkit/networks';
+import { createAppKit } from 'https://jspm.dev/@reown/appkit@1.6.0';
+import { Ethers5Adapter } from 'https://jspm.dev/@reown/appkit-adapter-ethers5@1.6.0';
+import { bsc } from 'https://jspm.dev/@reown/appkit/networks';
 
 // ==========================================
 // VARIABLES GLOBALES DE ESTADO
